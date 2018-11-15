43067d7d6a1c98661ea61bfc4f0144369268fc50
BrianCoyle <30752555+BrianCoyle@users.noreply.github.com>
2018-11-15 10:19:36 +0000

    Add files via upload

--- train_plot.py
+++ train_plot.py
@@ -5,23 +5,24 @@ from sample_gen import BornSampler, PlusMinusSampleGen
 from train_generation import TrainingData, DataSampler
 from classical_kernel import GaussianKernel
 from mmd_kernel import KernelCircuit, KernelComputation, EncodingFunc
-from mmd_sampler2 import MMDKernel, MMDGrad, MMDCost
+from mmd_functions import MMDKernel, MMDGrad, MMDCost
+
 from cost_function_train import TrainBorn
+
 from file_operations_in import DataDictFromFile
 from auxiliary_functions import EmpiricalDist, SampleListToArray
 from stein_discrepancy import SteinTrain
 import sys
 
-def Plot(N, N_v, N_epochs, \
-            J_i, b_i, g_x_i, g_y_i, \
+def Plot(N, N_v, N_epochs, initial_params, \
             learning_rate, approx, kernel_type,\
             data_samples, data_exact_dict, \
             N_data_samples, N_born_samples, N_bornplus_samples, N_bornminus_samples, N_kernel_samples,\
             plot_colour, weight_sign, cost_func, score_approx):
     
     #Output MMD Loss function and parameter values, for given number of training samples
-    L_stein, L_var, L_mmd, J, b, gamma_x, gamma_y, born_probs_list, empirical_probs_dict = TrainBorn(N, N_v, cost_func,\
-                                                                            J_i, b_i, g_x_i, g_y_i, \
+    L_stein, L_var, L_mmd, circuit_params, born_probs_list, empirical_probs_dict = TrainBorn(N, N_v, cost_func,\
+                                                                            initial_params, \
                                                                             N_epochs, N_data_samples, N_born_samples,\
                                                                             N_kernel_samples, \
                                                                             data_samples, data_exact_dict, \
@@ -33,15 +34,15 @@ def Plot(N, N_v, N_epochs, \
                                         %(N_data_samples, N_born_samples, kernel_type[0], N_kernel_samples))
             # plt.plot(L_stein, 'bo-' , label ='Stein, %i Data Samples,  %i Born Samples for a %s kernel with %i Measurements.' \
             #                             %(N_data_samples, N_born_samples, kernel_type[0], N_kernel_samples))
-            plt.plot(L_var, '%so-' %(plot_colour[1]), label ='TV, %i Data Samples,  %i Born Samples for a %s kernel with %i Measurements.' \
-                                        %(N_data_samples, N_born_samples, kernel_type[0], N_kernel_samples))
+            # plt.plot(L_var, '%so-' %(plot_colour[1]), label ='TV, %i Data Samples,  %i Born Samples for a %s kernel with %i Measurements.' \
+            #                             %(N_data_samples, N_born_samples, kernel_type[0], N_kernel_samples))
         else:
             plt.plot(L_mmd,  '%so-' %(plot_colour[0]), label ='MMD, %i Data Samples,  %i Born Samples for a %s kernel.' \
                                         %(N_data_samples, N_born_samples, kernel_type[0]))
             # plt.plot(L_stein, 'bo-' , label ='Stein, %i Data Samples,  %i Born Samples for a %s kernel.' \
             #                             %(N_data_samples, N_born_samples, kernel_type[0]))
-            plt.plot(L_var, '%so-' %(plot_colour[1]), label ='TV, %i Data Samples,  %i Born Samples for a %s kernel.' \
-                                        %(N_data_samples, N_born_samples, kernel_type[0]))
+            # plt.plot(L_var, '%so-' %(plot_colour[1]), label ='TV, %i Data Samples,  %i Born Samples for a %s kernel.' \
+            #                             %(N_data_samples, N_born_samples, kernel_type[0]))
     elif (approx == 'Exact'):
         if kernel_type == 'Quantum':
             plt.plot(L_mmd, '%so-' %(plot_colour[0]), label ='Exact %s kernel with $\eta$  = %.4f.' \
@@ -53,9 +54,9 @@ def Plot(N, N_v, N_epochs, \
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
     plt.xlabel("Epochs")
-    plt.ylabel("MMD Loss")
-    plt.title("MMD Loss for %i qubits" % N_v)
+    plt.ylabel("Loss")
+    plt.title("Loss for %i qubits" % N_v)
 
-    return L_stein, L_var, L_mmd , J, b, gamma_x, gamma_y, born_probs_list, empirical_probs_dict
+    return L_stein, L_var, L_mmd , circuit_params, born_probs_list, empirical_probs_dict
 
 
